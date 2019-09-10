26a3e90e9b8d95bb37bc64e91fde56e497ba794b
Nicolas Kircher <nicolas.kircher5@etu.univ-lorraine.fr>
2019-09-10 11:13:11 +0200

    Hello World fonctionne

--- src/public/index.php
+++ src/public/index.php
@@ -3,13 +3,13 @@
 require_once '../../vendor/autoload.php';
 
 use Illuminate\Database\Capsule\Manager as Manager;
-use Slim\app as App;
+use Slim\App as App;
 
 $db = new Manager();
 
 $app = new App();
 
 $app->get('/test', function(){
-    echo 'Hello World';
+    echo "Hello World";
 });
-
+$app->run();

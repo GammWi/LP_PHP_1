bae41cc692d3ca973c0c744193a49c2272d40dae
Nicolas Kircher <nicolas.kircher5@etu.univ-lorraine.fr>
2019-09-24 11:15:54 +0200

    Debut controller connexion

--- src/public/Controllers/userController.php
+++ src/public/Controllers/userController.php
@@ -1,1 +1,18 @@
 <?php
+
+namespace dawa\controllers;
+
+class userController{
+
+    public static function authentification($username, $password){
+        $u = User::where('username', '=', $username)->first();
+        if(password_verify($password, $u->password)){
+            $_SESSION['idUser'] = $u->id;
+            $_SESSION['statut'] = 'connected';
+            return 0;
+        }else{
+            $_SESSION['statut'] = 'failed';
+            return -1;
+        }
+    }
+}
--- src/public/Models/user.php
+++ src/public/Models/user.php
@@ -1,1 +1,13 @@
 <?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model{
+
+    protected $table = "user";
+    protected $fillable = ['username', "password"];
+
+    protected $primaryKey = "id";
+
+    public $timestamps = true;
+}

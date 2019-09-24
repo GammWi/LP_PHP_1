3c9803956647c3c79f48aed9effcc109cce82a66
Lucas <lucas.ligerot5u@etu.univ-lorraine.fr>
2019-09-24 11:03:57 +0200

    version pour les pd

--- bdd.sql
+++ bdd.sql
@@ -1,53 +1,146 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mar. 24 sep. 2019 à 09:03
+-- Version du serveur :  5.7.19
+-- Version de PHP :  7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de données :  `lp_projet_php`
+--
 
-CREATE Table element (id_element serial not null auto_increment,
-name Varchar(55) not null,
-description varchar(255),
-PRIMARY KEY (id_element),
-unique key(name));
+-- --------------------------------------------------------
 
-create table isStrongerElem (id_isStronger serial not null auto_increment,
-  id_elem_stronger int not null,
-  id_elem_weaker int not null,
-  primary key (id_isStronger),
-  foreign key (id_elem_stronger) references element(id_element),
-  foreign key (id_elem_weaker) references element(id_element));
+--
+-- Structure de la table `element`
+--
 
-create table race(id_race serial not null auto_increment,
-  name varchar(55),
-  description varchar(255),
-  boostAttack float,
-  boostDefense float,
-  boostAgility float,
-  boostHp float,
-  primary key (id_race)
+DROP TABLE IF EXISTS `element`;
+CREATE TABLE IF NOT EXISTS `element` (
+  `id_element` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(55) COLLATE utf8_bin NOT NULL,
+  `description` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id_element`),
+  UNIQUE KEY `id_element` (`id_element`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-);
+-- --------------------------------------------------------
 
-create table personnage(id_personnage serial not null auto_increment,
-  id_personnage_race int not null,
-  name varchar(55) not null,
-  picture varchar(155),
-  primary key (id_personnage),
-  foreign key (id_personnage_race) references race(id_race));
+--
+-- Structure de la table `hero`
+--
 
-create table hero(id_hero serial not null auto_increment,
-  id_personnage int not null,
-  primary key (id_hero),
-  unique key(id_personnage),
-  firstname varchar(55));
+DROP TABLE IF EXISTS `hero`;
+CREATE TABLE IF NOT EXISTS `hero` (
+  `id_hero` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_personnage` int(11) NOT NULL,
+  `firstname` varchar(55) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id_hero`),
+  UNIQUE KEY `id_hero` (`id_hero`),
+  UNIQUE KEY `id_personnage` (`id_personnage`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
+-- --------------------------------------------------------
 
-create table monstre(id_monstre serial not null auto_increment,
-  id_personnage int not null,
-  primary key (id_monstre),
-  unique key(id_personnage),
-  firstname varchar(55));
+--
+-- Structure de la table `isstrongerelem`
+--
 
-create table user(id_user serial not null auto_increment,
-  username varchar(55),
-  password varchar(255),
-  primary key (id_user));
+DROP TABLE IF EXISTS `isstrongerelem`;
+CREATE TABLE IF NOT EXISTS `isstrongerelem` (
+  `id_isStronger` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_elem_stronger` int(11) NOT NULL,
+  `id_elem_weaker` int(11) NOT NULL,
+  PRIMARY KEY (`id_isStronger`),
+  UNIQUE KEY `id_isStronger` (`id_isStronger`),
+  KEY `id_elem_stronger` (`id_elem_stronger`),
+  KEY `id_elem_weaker` (`id_elem_weaker`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `monstre`
+--
+
+DROP TABLE IF EXISTS `monstre`;
+CREATE TABLE IF NOT EXISTS `monstre` (
+  `id_monstre` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_personnage` int(11) NOT NULL,
+  `firstname` varchar(55) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id_monstre`),
+  UNIQUE KEY `id_monstre` (`id_monstre`),
+  UNIQUE KEY `id_personnage` (`id_personnage`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `personnage`
+--
+
+DROP TABLE IF EXISTS `personnage`;
+CREATE TABLE IF NOT EXISTS `personnage` (
+  `id_personnage` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_personnage_race` int(11) NOT NULL,
+  `name` varchar(55) COLLATE utf8_bin NOT NULL,
+  `picture` varchar(155) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id_personnage`),
+  UNIQUE KEY `id_personnage` (`id_personnage`),
+  KEY `id_personnage_race` (`id_personnage_race`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `race`
+--
+
+DROP TABLE IF EXISTS `race`;
+CREATE TABLE IF NOT EXISTS `race` (
+  `id_race` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(55) COLLATE utf8_bin DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `boostAttack` float DEFAULT NULL,
+  `boostDefense` float DEFAULT NULL,
+  `boostAgility` float DEFAULT NULL,
+  `boostHp` float DEFAULT NULL,
+  PRIMARY KEY (`id_race`),
+  UNIQUE KEY `id_race` (`id_race`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(55) COLLATE utf8_bin DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `id_user` (`id_user`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
 

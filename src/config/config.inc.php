32c4e252ed796cfb6f6a092e6ead55fcf4519806
Theo1809 <55275426+Theo1809@users.noreply.github.com>
2019-10-01 11:35:06 +0200

    Update config.inc.php

--- src/config/config.inc.php
+++ src/config/config.inc.php
@@ -1,5 +1,18 @@
 <?php
 global $BASE_URL;
 
+$config=array();
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db'] = [
+    'driver'    => 'mysql',
+    'host'      => "localhost",
+    'database'  => "lp_projet_php",
+    'username'  => "root",
+    'password'  => "",
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
 
 ];

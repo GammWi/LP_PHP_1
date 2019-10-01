08a4125ab17732be05b8a1a78ded607493033250
Lucas <lucas.ligerot5u@etu.univ-lorraine.fr>
2019-10-01 08:41:15 +0200

    some modif

--- src/public/Controllers/heroController.php
+++ src/public/Controllers/heroController.php
@@ -1,1 +1,20 @@
 <?php
+namespace dawa\controllers;
+
+class heroController{
+
+    public function Index(){
+        echo 'HERE WE ARE';
+    }
+    public static function authentification($username, $password){
+        $u = User::where('username', '=', $username)->first();
+        if(password_verify($password, $u->password)){
+            $_SESSION['idUser'] = $u->id;
+            $_SESSION['statut'] = 'connected';
+            return 0;
+        }else{
+            $_SESSION['statut'] = 'failed';
+            return -1;
+        }
+    }
+}

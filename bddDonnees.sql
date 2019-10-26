686b9d00cb6f2e8b56462a3b4906de9fc2183f3c
Théo <theo.fraschini8@etu.univ-lorraine.fr>
2019-10-26 11:00:59 +0200

    modif bdd pour nouveaux persos

--- bddDonnees.sql
+++ bddDonnees.sql
@@ -1,25 +1,25 @@
 INSERT INTO `character` (`id_character_race`, `id_character_elem`, `name`, `picture`) VALUES
 (1, 1, 'Nain', 1),
-(1, 4, 'Nanino', 14),
+(1, 4, 'Nanino', 13),
 (2, 3, 'Ranger', 2),
 (2, 1, 'Barbare', 3),
-(2, 1, 'Paladin', 15),
-(2, 4, 'Guerrier', 16),
+(2, 1, 'Paladin', 14),
+(2, 4, 'Guerrier', 15),
 (3, 2, 'Elfe', 4),
-(3, 3, 'Guerriere', 17),
+(3, 3, 'Guerriere', 16),
 (4, 1, 'Ogre', 5),
-(4, 2, 'Ogrito', 18),
+(4, 2, 'Ogrito', 17),
 (5, 4, 'Orque', 6),
-(5, 2, 'Orque marin', 20),
+(5, 2, 'Orque marin', 19),
 (6, 1, 'Gobelin', 7),
-(6, 2, 'Armored Gobelin', 23),
+(6, 2, 'Armored Gobelin', 22),
 (7, 2, 'Troll', 8),
 (8, 1, 'Mort-vivant', 9),
-(8, 4, 'Zombie', 22),
+(8, 4, 'Zombie', 21),
 (9, 1, 'Squelette', 10),
-(9, 3, 'Armored skeleton', 21),
+(9, 3, 'Armored skeleton', 20),
 (2, 1, 'Sorcier', 11),
-(2, 4, 'Dark warrior', 19);
+(2, 4, 'Dark warrior', 18);
 
 INSERT INTO `element` (`id_element`, `name`, `description`) VALUES
 (1, 'Feu', 'Le feu ça brûle'),
@@ -81,7 +81,7 @@ INSERT INTO `pictures` (`name`, `path`) values
 ("Orque marin", "../../public/assets/img/characters/Orque marin.jpeg"),
 ("Armored Skeleton", "../../public/assets/img/characters/Armored Skeleton.jpeg"),
 ("Zombie", "../../public/assets/img/characters/Zombie.jpeg"),
-("Armored Gobelin", "../../public/assets/img/characters/Armored Gobelin");
+("Armored Gobelin", "../../public/assets/img/characters/Armored Gobelin.png");
 
 
 INSERT INTO `race` (`id_race`, `name`, `description`, `attack`, `defense`, `agility`, `hp`, `id_picture`) VALUES

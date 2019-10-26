69e3c4bfac6693b865637efc01c70ce7274a5b6a
Théo <theo.fraschini8@etu.univ-lorraine.fr>
2019-10-26 11:00:18 +0200

    ajout personnages

--- bddDonnees.sql
+++ bddDonnees.sql
@@ -1,16 +1,25 @@
 INSERT INTO `character` (`id_character_race`, `id_character_elem`, `name`, `picture`) VALUES
 (1, 1, 'Nain', 1),
-(2, 1, 'Ranger', 2),
-(2, 1, 'Voleur', 3),
-(2, 1, 'Barbare', 4),
-(3, 1, 'Elfe', 5),
-(4, 1, 'Ogre', 6),
-(5, 1, 'Orque', 7),
-(6, 1, 'Gobelin', 8),
-(7, 1, 'Troll', 9),
-(8, 1, 'Mort-vivant', 10),
-(9, 1, 'Squelette', 11),
-(2, 1, 'Sorcier', 12);
+(1, 4, 'Nanino', 14),
+(2, 3, 'Ranger', 2),
+(2, 1, 'Barbare', 3),
+(2, 1, 'Paladin', 15),
+(2, 4, 'Guerrier', 16),
+(3, 2, 'Elfe', 4),
+(3, 3, 'Guerriere', 17),
+(4, 1, 'Ogre', 5),
+(4, 2, 'Ogrito', 18),
+(5, 4, 'Orque', 6),
+(5, 2, 'Orque marin', 20),
+(6, 1, 'Gobelin', 7),
+(6, 2, 'Armored Gobelin', 23),
+(7, 2, 'Troll', 8),
+(8, 1, 'Mort-vivant', 9),
+(8, 4, 'Zombie', 22),
+(9, 1, 'Squelette', 10),
+(9, 3, 'Armored skeleton', 21),
+(2, 1, 'Sorcier', 11),
+(2, 4, 'Dark warrior', 19);
 
 INSERT INTO `element` (`id_element`, `name`, `description`) VALUES
 (1, 'Feu', 'Le feu ça brûle'),
@@ -19,12 +28,16 @@ INSERT INTO `element` (`id_element`, `name`, `description`) VALUES
 (4, 'Terre', 'La terre s\'atterre');
 
 INSERT INTO `hero` (`id_character`, `firstname`) VALUES
-(1, 'Nano Le'),
-(2, 'Roger Le'),
-(3, 'Val Le '),
-(4, 'Borbert Le '),
-(5, 'Elfora L\''),
-(6, 'Augro L\'');
+(1, 'Nano'),
+(2, 'El'),
+(3, 'Roger'),
+(4, 'Borbert'),
+(5, 'Palau'),
+(6, 'Guerric'),
+(7, 'Elfora'),
+(8, 'Audrey'),
+(9, 'Augro'),
+(10, 'Greg');
 
 INSERT INTO `isstrongerelem` (`id_isStronger`, `id_elem_stronger`, `id_elem_weaker`) VALUES
 (1, 2, 1),
@@ -33,18 +46,22 @@ INSERT INTO `isstrongerelem` (`id_isStronger`, `id_elem_stronger`, `id_elem_weak
 (4, 3, 2);
 
 INSERT INTO `monster` (`id_character`) VALUES
-(7),
-(8),
-(9),
-(10),
 (11),
-(12);
+(12),
+(13),
+(14),
+(15),
+(16),
+(17),
+(18),
+(19),
+(20),
+(21);
 
 
 INSERT INTO `pictures` (`name`, `path`) values
 ("nain", "../../public/assets/img/characters/nain.png"),
 ("ranger", "../../public/assets/img/characters/ranger.jpg"),
-("voleur", "../../public/assets/img/characters/voleur.jpg"),
 ("barbare", "../../public/assets/img/characters/barbare.jpg"),
 ("elfe","../../public/assets/img/characters/elfe.jpg"),
 ("ogre","../../public/assets/img/characters/ogre.jpg"),
@@ -54,18 +71,29 @@ INSERT INTO `pictures` (`name`, `path`) values
 ("mort","../../public/assets/img/characters/mort.jpg"),
 ("squelette","../../public/assets/img/characters/squelette.jpg"),
 ("sorcier", "../../public/assets/img/characters/sorcier.jpg"),
-("humain", "../../public/assets/img/characters/humain.jpg");
+("humain", "../../public/assets/img/characters/humain.jpg"),
+("Nanino", "../../public/assets/img/characters/Nanino.png"),
+("Paladin", "../../public/assets/img/characters/Paladin.png"),
+("Guerrier", "../../public/assets/img/characters/Guerrier.jpeg"),
+("Guerrière", "../../public/assets/img/characters/Guerrière.jpeg"),
+("Ogrito", "../../public/assets/img/characters/Ogrito.jpeg"),
+("Dark warrior", "../../public/assets/img/characters/Dark warrior.jpeg"),
+("Orque marin", "../../public/assets/img/characters/Orque marin.jpeg"),
+("Armored Skeleton", "../../public/assets/img/characters/Armored Skeleton.jpeg"),
+("Zombie", "../../public/assets/img/characters/Zombie.jpeg"),
+("Armored Gobelin", "../../public/assets/img/characters/Armored Gobelin");
+
 
 INSERT INTO `race` (`id_race`, `name`, `description`, `attack`, `defense`, `agility`, `hp`, `id_picture`) VALUES
-(1, 'Nain', 'Les nains sont des créatures robustes de petite taille, qui se distinguent par leurs talents de forgerons, de mineurs et de bâtisseurs. Ils adorent la bière et détestent les elfes', 20, 25, 5, 70,1),
-(2, 'Humain', 'Race qui a inventé la bière et la raclette. Peut aussi réussir les captcha', 15, 15, 20, 50,2),
-(3, 'Elfe', 'Les elfes sont une race ancienne, et sont en communion avec la nature. Ils aiment se balader en forêt, insulter des nains et faire des tresses aux poneys. Ah, et ils mangent de la salade aussi', 15, 10, 25, 40,3),
-(4, 'Ogre', 'Les ogres sont des créatures massives connues pour leur appétit insasiable et leur intelligence limité. Malgré cela ils sont d\'une loyauté indefectible envers ceux qui arrivent à parler leur étrange language (du moins tant qu\'il y a assez à manger)', 20, 10, 5, 100, 4),
-(5, 'Orque', 'Les orques sont des créatures redoutables pour des aventuriers mal prépaprés. Ils servent d\'armée de base aux seigneurs maléfiques et sorciers pour leurs donjons, du fait de leur robustesse et violence au combat (mais ils sont un peu débiles quand même)', 15, 15, 10, 60, 5),
-(6, 'Gobelin', 'Les gobelins sont une race petite et espiègle, qui peuvent parraître inoffensifs à première vue, mais faites attention à ne jamais sous-estimer une bande de gobelins enragés!', 15, 5, 30, 30, 6),
-(7, 'Troll', 'Les trolls ressemblent beaucoup aux ogres de part leur silhouette imposante, mais contrairement à leurs cousins pacifistes, ils ne vivent que pour la guerre', 25, 5, 5, 100, 7),
-(8, 'Mort-vivant', 'Les Mort-vivants sont des cadavres ramenés à la vie par des sorciers nécromanciens. Ils ont la vitesse et l\'intelligence d\'un escargot alcoolisé mais il ne faut pas sous-estimer le danger qu\'ils représentent quand ils sont nombreux', 15, 20, 5, 70, 8),
-(9, 'Squelette', 'Plus intelligents et plus que les morts-vivants, les squelettes sont nimbés de mystères: comment se nourissent-ils? Comment font-ils en cas de fracture? Prennent-ils des bains de lait pour fortifier leurs os?', 20, 5, 20, 40, 9);
+(1, 'Nain', 'Les nains sont des créatures robustes de petite taille, qui se distinguent par leurs talents de forgerons, de mineurs et de bâtisseurs. Ils adorent la bière et détestent les elfes', 25, 25, 5, 70,1),
+(2, 'Humain', 'Race qui a inventé la bière et la raclette. Peut aussi réussir les captcha', 15, 15, 20, 50,13),
+(3, 'Elfe', 'Les elfes sont une race ancienne, et sont en communion avec la nature. Ils aiment se balader en forêt, insulter des nains et faire des tresses aux poneys. Ah, et ils mangent de la salade aussi', 15, 10, 25, 40,5),
+(4, 'Ogre', 'Les ogres sont des créatures massives connues pour leur appétit insasiable et leur intelligence limité. Malgré cela ils sont d\'une loyauté indefectible envers ceux qui arrivent à parler leur étrange language (du moins tant qu\'il y a assez à manger)', 20, 10, 5, 100, 6),
+(5, 'Orque', 'Les orques sont des créatures redoutables pour des aventuriers mal prépaprés. Ils servent d\'armée de base aux seigneurs maléfiques et sorciers pour leurs donjons, du fait de leur robustesse et violence au combat (mais ils sont un peu débiles quand même)', 15, 15, 10, 60, 7),
+(6, 'Gobelin', 'Les gobelins sont une race petite et espiègle, qui peuvent parraître inoffensifs à première vue, mais faites attention à ne jamais sous-estimer une bande de gobelins enragés!', 15, 5, 50, 30, 8),
+(7, 'Troll', 'Les trolls ressemblent beaucoup aux ogres de part leur silhouette imposante, mais contrairement à leurs cousins pacifistes, ils ne vivent que pour la guerre', 15, 5, 5, 100, 9),
+(8, 'Mort-vivant', 'Les Mort-vivants sont des cadavres ramenés à la vie par des sorciers nécromanciens. Ils ont la vitesse et l\'intelligence d\'un escargot alcoolisé mais il ne faut pas sous-estimer le danger qu\'ils représentent quand ils sont nombreux', 25, 20, 10, 70, 10),
+(9, 'Squelette', 'Plus intelligents et plus que les morts-vivants, les squelettes sont nimbés de mystères: comment se nourissent-ils? Comment font-ils en cas de fracture? Prennent-ils des bains de lait pour fortifier leurs os?', 20, 5, 15, 40, 11);
 
 -- REQUETE COMPTE ADMIN --
 -- ID: root
